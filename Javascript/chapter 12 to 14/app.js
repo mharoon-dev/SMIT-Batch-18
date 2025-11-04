93b9cab918e290f579aca6ddee1cf232cc8c7178
mharoon-dev <haroon.aghani@gmail.com>
2025-11-04 21:43:05 +0500

    chapter 12 to 14

---
+++ Javascript/chapter 12 to 14/app.js
@@ -0,0 +1,96 @@
+// console.log("hello")
+
+// var question = prompt("what is your role ?")
+// console.log(question)
+
+// if (question === "haroon") {
+//     alert("this is trainer!")
+// } else {
+//     alert("this is student!")
+// }
+
+// if (question !== "haroon") {
+//     alert("this is student!")
+// }
+
+// trainer
+// admin
+// watchMen
+// student
+
+// if (question === "haroon") {
+//     alert("this is trainer!")
+// } else if (question === "owais") {
+//     alert("this is admin!")
+// }else if (question === "watch men") {
+//     alert("this is watch men")
+// } else {
+//     alert('this is student!')
+// }
+
+// var idNumber = prompt("Please Enter Your ID.")
+
+// if (idNumber == 448856) {
+//     console.log("ahamd ki attendance mark ho gai")
+// } else if (idNumber == 445982) {
+//     console.log("arsalan ki attendance mark ho gai")
+// }
+//  else if (idNumber == 456087) {
+//     console.log("hammad ki attendance mark ho gai")
+// } else {
+//     console.log("Invalid roll number!")
+// }
+
+// chapter 13
+// var gender = "female";
+// var age = 15;
+// var weight = 100;
+
+// if (gender == "male" && age > 18 && weight < "90kg") {
+//     console.log("you are perfect")
+// } else {
+//     console.log("you are not perfect")
+// }
+
+// if (gender == "male" || age > 18 || weight < 90) {
+//     console.log("you are perfect")
+// } else {
+//     console.log("you are not perfect")
+// }
+
+// if (gender == "female" || (age == 15 && weight == 100)) {
+//   console.log("ok");
+// } else {
+//   console.log("not ok ");
+// }
+
+var a = 10;
+var b = 20;
+var c = 20;
+var d = 30;
+var e = 30;
+var f = 50;
+var g = 50;
+var h = 60;
+
+if (a == a) {
+  if (b == d) {
+    if (d == h) {
+      console.log("d and h are equal");
+    } else if (d == e) {
+      console.log("d and e are equal");
+    } else {
+      console.log("no equal value found ");
+    }
+  } else if (b == c) {
+    if (a == h) {
+      console.log("a and h are equal");
+    } else if (f == g) {
+      console.log("f and g are equal");
+    } else {
+      console.log("no equal value found ");
+    }
+  }
+} else {
+  console.log("a is not equal to a");
+}
