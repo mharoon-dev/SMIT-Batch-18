501763acaa823c9591f12bda10d7907a9d52f5ed
mharoon-dev <haroon.aghani@gmail.com>
2025-11-08 21:56:30 +0500

    chapter till 18

---
+++ Javascript/chapter 15/app.js
@@ -0,0 +1,50 @@
+// console.log("ok")
+
+// var stu1 = "faheem";
+// var stu2 = "hammad";
+// var stu3 = "hamza";
+// var stu4 = "ahmad";
+// var stu5 = "khan";
+// var stu6 = "talha";
+// var stu7 = "rafay";
+// var stu8 = "abdul hadi";
+// var stu9 = "anas";
+// var stu10 = "haseeb";
+// var stu11 = "ahmad raza";
+// var stu12 = "zohaib";
+
+// var allStu = [
+//   "faheem",
+//   "hammad",
+//   45,
+//   true,
+//   false,
+//   "hamza",
+//   "ahmad",
+//   "khan",
+//   "talha",
+//   "rafay",
+//   "abdul hadi",
+//   "anas",
+//   "haseeb",
+//   "ahmad raza",
+//   stu12,
+// ];
+
+// console.log(allStu[0]);
+
+// boolean (true false)
+
+var cities = ["karachi", "lahore", "islamabad"];
+
+var cityName = prompt("write your city name?");
+
+if (cityName === cities[0]) {
+  alert("welcome to " + cities[0]);
+} else if (cityName === cities[1]) {
+  alert("welcome to " + cities[1]);
+} else if (cityName === cities[2]) {
+  alert("welcome to " + cities[2]);
+} else {
+  alert("city not found!");
+}
---
+++ Javascript/chapter 16 to 18/app.js
@@ -0,0 +1,142 @@
+var fruits = ["apple", "banana ", "strewbary", "orange"];
+console.log(fruits);
+
+// fruits.unshift("mango", "watermelon", true, 45);
+// console.log(fruits);
+
+// fruits.shift();
+
+// console.log(fruits);
+
+fruits.push("mango", true, 45);
+console.log(fruits);
+
+fruits.pop();
+
+console.log(fruits);
+// array ke start me data add kar na hai (unshift)
+// array ke start se data nikal na hai (shift)
+// array ke end me data add kar na hai (push)
+// array ke end se data nikal na hai  (pop)
+
+var cities = [
+  "Karachi",
+  "Lahore",
+  "Islamabad",
+  "Rawalpindi",
+  "Faisalabad",
+  "Multan",
+  "Peshawar",
+  "Quetta",
+  "Sialkot",
+  "Gujranwala",
+  "Hyderabad",
+  "Sukkur",
+  "Bahawalpur",
+  "Sargodha",
+  "Larkana",
+  "Sheikhupura",
+  "Mardan",
+  "Abbottabad",
+  "Jhang",
+  "Kasur",
+  "Rahim Yar Khan",
+  "Okara",
+  "Dera Ghazi Khan",
+  "Chiniot",
+  "Gujrat",
+  "Mirpur",
+  "Mingora",
+  "Nawabshah",
+  "Turbat",
+  "Charsadda",
+  "Muzaffarabad",
+  "Kohat",
+  "Bannu",
+  "Sahiwal",
+  "Jacobabad",
+  "Kandhkot",
+  "Hafizabad",
+  "Tando Allahyar",
+  "Tando Adam",
+  "Dera Ismail Khan",
+  "Khuzdar",
+  "Shikarpur",
+  "Nowshera",
+  "Muridke",
+  "Pakpattan",
+  "Jhelum",
+  "Kamoke",
+  "Mandi Bahauddin",
+  "Khanewal",
+  "Swabi",
+  "Chakwal",
+  "Haripur",
+  "Vehari",
+  "Mansehra",
+  "Attock",
+  "Badin",
+  "Ghotki",
+  "Bhakkar",
+  "Khairpur",
+  "Daska",
+  "Layyah",
+  "Mianwali",
+  "Tando Muhammad Khan",
+  "Thatta",
+  "Kotri",
+  "Dera Allah Yar",
+  "Umerkot",
+  "Gwadar",
+  "Hub",
+  "Zhob",
+  "Parachinar",
+  "Kharian",
+  "Hangu",
+  "Tank",
+  "Lodhran",
+  "Nankana Sahib",
+  "Narowal",
+  "Kohlu",
+  "Chaman",
+  "Kalat",
+  "Bhalwal",
+  "Shahdadpur",
+  "Jaranwala",
+  "Moro",
+  "Muzaffargarh",
+  "Kharan",
+  "Jatoi",
+  "Arifwala",
+  "Hala",
+  "Kundian",
+  "Kandhkot",
+  "Kot Addu",
+  "dadu",
+  "Khairpur Nathan Shah",
+  "Jamshoro",
+  "Sanghar",
+  "Skardu",
+  "Gilgit",
+  "Chilas",
+  "Hunza",
+];
+
+// var question = prompt("write your city name!");
+
+// for (i = 0; i <= cities.length; i++) {
+//   if (question.v() == cities[i]) {               // jatoi == cities[1] // DaDU ==>>> dadu
+//     console.log(cities[i] + " found");
+//   } else {
+//     console.log('not found')
+//   }
+// }
+
+// console.log(x);
+
+// loop chale ga agar condition true hoi
+// execution chale gi (execution = {})
+// i++
+
+var name = "HaRoOn";
+console.log(name.toUpperCase());
