b9794ffe96e682e7282c9ec1bd9dd7b7498c6028
mharoon-dev <haroon.aghani@gmail.com>
2025-11-11 21:40:04 +0500

    chapter 19 to 20

---
+++ Javascript/chapter 19 to 20/app.js
@@ -0,0 +1,48 @@
+// var askNumber = prompt("tell me one number in digit!");
+// console.log(askNumber);
+
+// if (isNaN(askNumber)) {
+//   askNumber = prompt("write a valid number not an alphabet!");
+// } else if (askNumber === "") {
+//   askNumber = prompt("write a valid number!");
+// }
+
+// for (i = 1; i <= 10; i++) {
+//   var multipliedNum = askNumber * i;
+//   document.write(askNumber + "x" + i + "=" + multipliedNum + "<br>");
+// }
+
+// for (i = 0; i <= 25; i++) {
+//   if (i < 15) {
+//     console.log(i);
+//     console.log("i is less then 15");
+//     break;
+//     // console.log("not working because loop is terminated!")
+//   } else {
+//     console.log("i is greater then 15 ");
+//   }
+// }
+
+// var firstNames = ["talha ", "Muhammad ", "muhammad ", "muhammad ", "haroon "];
+// var lastNames = ["zeeshan", "faheem", "arsalan", "ahmad"];
+// var fullNames = [];
+// var j = 0;
+// for (var i = 0; i < firstNames.length; i++) {
+//   for (j; j < lastNames.length; j++) {
+//     fullNames.push(firstNames[i] + lastNames[j]);
+//     j++;
+//     console.log(fullNames);
+//     break;
+//   }
+// }
+
+// console.log(fullNames.length)
+
+// loop chale ga agar condition true hoi
+// execution chale gi (execution = {})
+// i++ || j++
+
+var name = "HaRoOn";
+console.log(name);
+console.log(name.toLowerCase());
+console.log(name.toUpperCase());
