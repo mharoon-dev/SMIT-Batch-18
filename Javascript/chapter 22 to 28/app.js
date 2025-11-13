de1c26e615ad46e96f04805a5220d8f09085579e
mharoon-dev <haroon.aghani@gmail.com>
2025-11-13 22:03:27 +0500

    chapter 22 to 28

---
+++ Javascript/chapter 22 to 28/app.js
@@ -0,0 +1,48 @@
+// var stuNames = [
+//   "faHeeM",
+//   "waSiF",
+//   "taLhA",
+//   "raFaY",
+//   "ahMED",
+//   "abDuL",
+//   "arSalAn",
+// ];
+
+// // var newStu = ["faheem", "wasif", "talha", "rafay"];
+
+// var newStu = stuNames.slice(0, 4);
+// console.log(stuNames);
+// console.log(newStu);
+
+// stuNames.splice(4, 1 , newStu );
+// console.log(stuNames);
+
+// var text = "this is simple text simple simple";
+// console.log(text.indexOf("simple"));
+// console.log(text.lastIndexOf("simple"));
+// console.log(text.charAt(15));
+// console.log(text.replace("simple", "hard"));
+// console.log(text.replace(/simple/g, "hard"));
+// console.log(text.replaceAll("simple", "hard"));
+
+// var num = 45.75;
+// num = Math.round(num-1)
+// console.log(num);
+// num = Math.floor(45.9)
+// console.log(num);
+// num = Math.ceil(45.1)
+// console.log(num);
+
+// var randomNum = Math.random() * 6;
+// console.log(randomNum);
+// var roundOff = Math.ceil(randomNum);
+// console.log(roundOff);
+
+var askAgeToUser = prompt("write your age!");
+
+// var convertedAge = parseInt(askAgeToUser);
+var convertedAge = Number(askAgeToUser);
+console.log(askAgeToUser);
+
+var myFractional = parseFloat("1.9999");
+console.log(myFractional)
